d33ef855c3c276cc3269a67d90019ef1bc25d76e
Connor Turland <connorturland@gmail.com>
2018-01-16 15:39:29 -0500

    build the ES6 js file so you can require it

--- index.js
+++ index.js
@@ -1,53 +1,72 @@
-import axios from 'axios'
+'use strict';
 
-export function requestSendingMiddleware (store) {
-  return next => action => {
-    const { payload, meta, type } = action
-    // if its a network request
-    // fire an action that indicates the request has
-    // been sent
-    if (payload && payload.then) {
-      const requestSendingAction = {
-        payload: null,
-        type: type + 'Sent',
-        meta
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+exports.requestSendingMiddleware = requestSendingMiddleware;
+exports.send = send;
+exports.hcMiddleware = hcMiddleware;
+
+var _axios = require('axios');
+
+var _axios2 = _interopRequireDefault(_axios);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function requestSendingMiddleware(store) {
+  return function (next) {
+    return function (action) {
+      var payload = action.payload,
+          meta = action.meta,
+          type = action.type;
+      // if its a network request
+      // fire an action that indicates the request has
+      // been sent
+
+      if (payload && payload.then) {
+        var requestSendingAction = {
+          payload: null,
+          type: type + 'Sent',
+          meta: meta
+        };
+        store.dispatch(requestSendingAction);
       }
-      store.dispatch(requestSendingAction)
-    }
-    return next(action)
-  }
+      return next(action);
+    };
+  };
 }
 
-export function send (namespace, fnName, data) {
-    const stringified = typeof data === "object" ? JSON.stringify(data) : data
-    return axios.post(`/fn/${namespace}/${fnName}`, stringified)
-        .then(res => {
-            if (typeof res.data === "string" && res.data.indexOf("Error") > -1) {
-                return Promise.reject(new Error(res.data))
-            }
-            return Promise.resolve(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+function send(namespace, fnName, data) {
+  var stringified = (typeof data === 'undefined' ? 'undefined' : _typeof(data)) === "object" ? JSON.stringify(data) : data;
+  return _axios2.default.post('/fn/' + namespace + '/' + fnName, stringified).then(function (res) {
+    if (typeof res.data === "string" && res.data.indexOf("Error") > -1) {
+      return Promise.reject(new Error(res.data));
+    }
+    return Promise.resolve(res.data);
+  }).catch(function (err) {
+    console.log(err);
+  });
 }
 
-export function hcMiddleware (store) {
-    return next => action => {
-        const { type, meta } = action
-        if (!(meta && meta.isHc)) return next(action)
-        // the rest will be handled by redux-promises
-        let sendRequest = send(meta.namespace, type, meta.data)
-        sendRequest = meta.then ? sendRequest.then(meta.then) : sendRequest
-        const newAction = Object.assign({},
-          action,
-          {
-            payload: sendRequest
-          },
-          {
-            meta: Object.assign({}, meta, { isHc: false })
-          }
-        )
-        return next(newAction)
-    }
+function hcMiddleware(store) {
+  return function (next) {
+    return function (action) {
+      var type = action.type,
+          meta = action.meta;
+
+      if (!(meta && meta.isHc)) return next(action);
+      // the rest will be handled by redux-promises
+      var sendRequest = send(meta.namespace, type, meta.data);
+      sendRequest = meta.then ? sendRequest.then(meta.then) : sendRequest;
+      var newAction = Object.assign({}, action, {
+        payload: sendRequest
+      }, {
+        meta: Object.assign({}, meta, { isHc: false })
+      });
+      return next(newAction);
+    };
+  };
 }
---
+++ main.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+export function requestSendingMiddleware (store) {
+  return next => action => {
+    const { payload, meta, type } = action
+    // if its a network request
+    // fire an action that indicates the request has
+    // been sent
+    if (payload && payload.then) {
+      const requestSendingAction = {
+        payload: null,
+        type: type + 'Sent',
+        meta
+      }
+      store.dispatch(requestSendingAction)
+    }
+    return next(action)
+  }
+}
+
+export function send (namespace, fnName, data) {
+    const stringified = typeof data === "object" ? JSON.stringify(data) : data
+    return axios.post(`/fn/${namespace}/${fnName}`, stringified)
+        .then(res => {
+            if (typeof res.data === "string" && res.data.indexOf("Error") > -1) {
+                return Promise.reject(new Error(res.data))
+            }
+            return Promise.resolve(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+export function hcMiddleware (store) {
+    return next => action => {
+        const { type, meta } = action
+        if (!(meta && meta.isHc)) return next(action)
+        // the rest will be handled by redux-promises
+        let sendRequest = send(meta.namespace, type, meta.data)
+        sendRequest = meta.then ? sendRequest.then(meta.then) : sendRequest
+        const newAction = Object.assign({},
+          action,
+          {
+            payload: sendRequest
+          },
+          {
+            meta: Object.assign({}, meta, { isHc: false })
+          }
+        )
+        return next(newAction)
+    }
+}
